0e240085a0be6906a294404159f83fefbe70ca5e
Mr.Nameless <150578322+Nameless146@users.noreply.github.com>
2024-06-01 19:57:26 +0600

    Add files via upload

---
+++ name.js
@@ -0,0 +1,63 @@
+//1.Create a variable called carName, assign the value Volvo to it.//
+var carName = "Volvo";
+//2.On one single line, declare three variables with the following names and values://
+let firstname="John";
+let lastname="Doe";
+let age=35;
+//3.Use the correct assignment operator that will result in x being 50 (same as x = x * y).//
+x = 10
+y = 5
+x *= y  # The value of x will be 50.
+//4.Use comments to describe the correct data type of the following variables://
+let length = 16; // data type: number
+let lastName = "Johnson"; // data type: string
+const x =  firstName: "John", lastName: "Doe" ; // data type: object
+//5.Execute the function named myFunction.//
+function myFunction() {
+    alert("Hello World!");
+  }
+  //after i run this code i see "hello world"//
+//6.Create an object called person with name = John, age = 50, Then, access the object to alert("John is 50").//
+var person = {
+    name: "John",
+    age: 50
+  };
+  
+  alert(person.name + " is " + person.age);
+  //when run this code i see "John is 50".//
+  //7.the answer is on html file//
+  //8.Array Related Question //
+  const Brand = ["Volvo", "Jeep", "Mercedes"];
+  Brand[0] = "Ford";
+  //After running this code, the Brand array will be ["Ford", "Jeep", "Mercedes"], with the first item changed to "Ford".//
+  //9.Math Related Problems//
+  //1. To create a random number//
+  const randomNumber = Math.random(); // create a random number between 0 (inclusive) and 1 (exclusive)
+  console.log(randomNumber);
+  //2. To return the largest number between 10 and 20//
+  const largestNumber = Math.max(10, 20); // Return  largest number between 10 to 20
+  console.log(largestNumber);
+  //3. To get the square root of 9//
+  const squareRootOf9 = Math.sqrt(9); // Return square root  9
+  console.log(squareRootOf9);
+  //10.comparison operator related problems! //
+  //1. To alert true when x is greater than y//
+  const x = 10;
+  const y = 5;
+
+  if (x > y) {
+  alert(true);
+  }
+  //2. To choose the correct conditional (ternary) operator to alert "Too young" if age is less than 18, otherwise alert "Old enough"//
+  const age = 16;
+
+ const message = age < 18 ? "Too young" : "Old enough";
+ alert(message);
+ 
+
+
+
+
+
+  
+  
